9c46626773b374d015c5f93d673ebe63882c6d70
Ville Andersson <winstoni@windowslive.com>
2021-11-02 00:00:19 +0200

    numeropeli

--- App.js
+++ App.js
@@ -1,13 +1,32 @@
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from 'react';
+import { StyleSheet, Text, View, Button, TextInput, Alert, } from 'react-native';
 
 export default function App() {
+  const [arvaus, setArvaus] = useState ("");
+  const [numero, setNumero] = useState (Math.floor(Math.random() * 100) + 1);
+  const [vastaus, setVastaus] = useState ("Arvaa numero 0 ja 100 väliltä");
+  let [maara, setMaara] = useState (0);
+  
+  function buttonPressed() {
+    setMaara(maara + 1);
+    if (parseInt(arvaus) == numero) {
+      Alert.alert("Oikein meni! Arvasit " + (maara + 1) + " kertaa.");
+    } else if (parseInt(arvaus) < numero) {
+      setVastaus("Arvaa isompaa");
+    } else {
+      setVastaus("Arvaa pienempää");
+    }
+  }
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+      <View style={styles.container}>
+        <Text>{vastaus}</Text>
+        <TextInput style={styles.input}onChangeText={arvaus=> setArvaus(arvaus)}value={arvaus} keyboardType="numeric"/>
+        <View style={styles.buttons}>
+          <Button onPress={buttonPressed} title="Arvaa" />
+        </View>
+      </View>
   );
 }
 
@@ -16,6 +35,18 @@ const styles = StyleSheet.create({
     flex: 1,
     backgroundColor: '#fff',
     alignItems: 'center',
-    justifyContent: 'center',
+    paddingTop: 150
   },
+  input: {
+    marginTop: 15,
+    width:200,
+    borderColor:'gray',
+    borderWidth:1
+  },
+  buttons: {
+    width: 60,
+    paddingTop: 15,
+    flexDirection: 'row',
+    justifyContent: 'space-between'
+  }
 });
